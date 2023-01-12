e349e5abde27b1f36925034b59cc11e5100b3601
Serdar İNCE <serdariince@gmail.com>
2023-01-13 01:11:34 +0300

    kutu

---
+++ ButtonsGame/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonsGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        private Button NewButton(int local)
+        {
+            var button = new Button
+            {
+                Location = new Point(local,local),
+            };
+            return button;
+        }
+        int local=0;
+        void EkleButton()
+        {
+            local += 25;
+            this.Controls.Add(NewButton(local));
+
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (local<=350)
+            {
+                EkleButton();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+            
+        }
+    }
+}
---
+++ KutuGames/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KutuGames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            EkleButton();
+        }
+        private Button NewButton()
+        {
+            var button = new Button();
+            return button;
+        }
+
+        void EkleButton(){
+            this.Controls.Add(NewButton());
+        }
+
+    }
+}
